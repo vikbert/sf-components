8282154e7519c99e86e3b9535264a7aa59641d84
Xun Zhou <segentor@gmail.com>
2021-02-06 15:33:32 +0100

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import FooterShape from './component/FooterShape.svelte';
-import Header from './component/Header.svelte';
+  import Header from './component/Header.svelte';
   import Components from './component/List.svelte';
 </script>
 
@@ -10,18 +10,8 @@ import Header from './component/Header.svelte';
 
 <main class="main">
   <Components />
-  <div class="copyright centered-xy">
-    <h5 class="opacity-75">vikbert © 2021</h5>
-  </div>
 </main>
 
 <FooterShape>
-  <div class="centered-xy">
-    vikbert ® 2021
-  </div>
+  <div class="centered-xy">vikbert ® 2021</div>
 </FooterShape>
-<style>
-  .copyright {
-    padding: 5rem 0;
-  }
-</style>
