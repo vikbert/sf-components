c768f5d1fe4340d1a4f99aad57bb551b09ddce7c
Xun Zhou <segentor@gmail.com>
2021-02-11 19:37:26 +0100

    adjust the footer icon size

--- src/footer/Footer.svelte
+++ src/footer/Footer.svelte
@@ -5,7 +5,6 @@
 <FooterShape>
     <div class="space-between">
         <div class="project-info">
-            <p>Vikbert ® 2021</p>
             <a href="https://github.com/vikbert/sf-components">
                 <img
                     src="https://img.shields.io/badge/License-MIT-brightgreen.svg?style=flat-square"
@@ -19,19 +18,25 @@
                     alt="Last Commit"
                 />
             </a>
+            <p>Vikbert ® 2021</p>
         </div>
         <div class="tech-info">
-            <a href="https://vikbert.github.io/pixss/"><img src="pixss.png" alt="pixss" width="54" class="pixss"/></a>
-            <a href="https://svelte.dev/"><img src="svelte.png" alt="made-with-svelte" width="180" /></a>
+            <a href="https://vikbert.github.io/pixss/"><img src="pixss.png" alt="pixss" width="34" class="pixss"/></a>
+            <a href="https://svelte.dev/"><img src="svelte.png" alt="made-with-svelte" width="120"/></a>
         </div>
     </div>
 </FooterShape>
 
 <style>
-    .project-info p {
-        margin-bottom: .5rem;
-    }
-    img.pixss {
-        filter: invert(1);
-    }
+  .project-info p {
+    margin-bottom: .5rem;
+  }
+
+  .tech-info {
+    padding-top: .5rem;
+  }
+
+  img.pixss {
+    filter: invert(1);
+  }
 </style>
